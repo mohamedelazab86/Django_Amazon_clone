ec21ecbc5c3e463b1fc235b09f6bc1c6a2a2f7d5
mohamedelazab86 <r_mido99@yahoo.com>
2023-12-06 17:53:18 +0200

    add inline tabularinline

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import Product,Brand,Review,ImageProduct
 
+
+
+# customize   product
+class Imgeadmininline(admin.TabularInline):
+    model=ImageProduct
+class Productadmin(admin.ModelAdmin):
+    list_display=['name','sku','flag']
+    list_filter=['price']
+    search_fields=['name','price']
+    inlines=[Imgeadmininline]
+
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product,Productadmin)
 admin.site.register(Brand)
 admin.site.register(Review)
-admin.site.register(ImageProduct)
+
